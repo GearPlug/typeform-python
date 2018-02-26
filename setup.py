870e07a390c297955d235cf2aba93b913b8d49df
Miguel Ferrer <ingferrermiguel@gmail.com>
2018-02-26 09:08:37 -0500

    Add MANIFEST.in; Bump ver. 0.1.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 
 setup(name='typeform-python',
-      version='0.1',
+      version='0.1.1',
       description='API wrapper for Typeform written in Python',
       long_description=read('README.md'),
       url='https://github.com/GearPlug/typeform-python',
